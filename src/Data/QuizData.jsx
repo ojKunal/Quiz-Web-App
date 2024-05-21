156d1a3edee11b0204218c74185428c73d852a5e
kunal ojha <ojhakunal9631@gmail.com>
2024-05-21 13:57:42 +0530

    new changes

--- src/Data/QuizData.jsx
+++ src/Data/QuizData.jsx
@@ -1,22 +1,52 @@
 export const QuizData = [
     {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
-        answer: 4
-    },
-    {
-        question: "What does CSS stand for?",
-        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        question: "What does CPU stand for?",
+        options: ["Central Process Unit", "Central Processing Unit", "Computer Personal Unit", "Central Processor Unit"],
         answer: 2
     },
     {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        question: "Which part of the computer is considered the 'brain'?",
+        options: ["RAM", "Hard Drive", "CPU", "Motherboard"],
+        answer: 3
+    },
+    {
+        question: "What does RAM stand for?",
+        options: ["Random Access Memory", "Readily Available Memory", "Read Access Memory", "Randomly Allocated Memory"],
         answer: 1
     },
     {
-        question: "What year was JavaScript launched?",
-        options: ["1996", "1995", "1994", "none of the above"],
-        answer:2
+        question: "What is the main function of the ALU (Arithmetic Logic Unit)?",
+        options: ["Performing arithmetic and logic operations", "Storing data", "Controlling computer operations", "Managing memory"],
+        answer: 1
     },
+    {
+        question: "Which device is used to convert digital signals to analog signals and vice versa?",
+        options: ["Router", "Modem", "Switch", "Hub"],
+        answer: 2
+    },
+    {
+        question: "What does BIOS stand for?",
+        options: ["Basic Integrated Operating System", "Basic Input Output System", "Basic Interactive Operating System", "Basic Internal Output System"],
+        answer: 2
+    },
+    {
+        question: "Which type of memory is non-volatile and can be electrically erased and reprogrammed?",
+        options: ["RAM", "ROM", "Cache Memory", "Flash Memory"],
+        answer: 4
+    },
+    {
+        question: "What is an example of an input device?",
+        options: ["Monitor", "Keyboard", "Printer", "Speaker"],
+        answer: 2
+    },
+    {
+        question: "What does GUI stand for?",
+        options: ["Graphical User Interface", "General User Interface", "Graphics User Input", "Geometric User Interface"],
+        answer: 1
+    },
+    {
+        question: "Which of the following is an operating system?",
+        options: ["Python", "Windows", "HTML", "Cisco"],
+        answer: 2
+    }
 ];
